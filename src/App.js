720df7cb2da50f47c5cde63a2768d2c1680e5662
ValGab <valentin.gabo@gmail.com>
2022-07-02 15:16:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import plus from "./assets/img/plus.png";
+import minus from "./assets/img/minus.png";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <div className="count">
+          {counter === 0 ? null : (
+            <img onClick={decrement} src={minus} alt="minus" />
+          )}
+          <div>
+            <p>{counter}</p>
+          </div>
+          {counter === 10 ? null : (
+            <img onClick={increment} src={plus} alt="plus" />
+          )}
+        </div>
+        <button onClick={reset} className="reset">
+          Reset
+        </button>
+      </main>
     </div>
   );
 }
